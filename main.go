1e1d9dd742f59f23afe906354beaeea961297a12
jmeisele <jmeisele@yahoo.com>
2022-05-24 05:22:24 -0500

    added simepl form and static pages

--- main.go
+++ main.go
@@ -10,20 +10,49 @@ func main() {
 	// Setup our server config
 	port := "8081"
 	host := "localhost"
-
+	fileServer := http.FileServer(http.Dir("./static"))
+	
 	// Add handlers for endpoints using callbacks
-	http.HandleFunc("/", homepage)
+	http.Handle("/", fileServer)
 	http.HandleFunc("/second", secondpage)
+	http.HandleFunc("/form", formHandler)
 
 	// Start the server
+	fmt.Println("Booting server at port", port)
 	err := http.ListenAndServe(host+":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-func homepage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the home page")
-}
+
 func secondpage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/second" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
 	fmt.Fprintf(w, "Welcome to the second page")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/form" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "POST" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s/n", name)
+	fmt.Fprintf(w, "Address = %s/n", address)
 }
